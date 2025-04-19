90a11fe6be78e678ed4d8e4c66af985d6373d160
NayanRaval <ravalnayan029@gmail.com>
2025-04-19 11:09:12 +0530

    feature : remove f key

--- database/migrations/2025_04_18_041638_create_saloon_wishlists_table.php
+++ database/migrations/2025_04_18_041638_create_saloon_wishlists_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('saloon_wishlists', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('saloon_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id');
+            $table->foreignId('saloon_id');
             $table->timestamps();
             $table->unique(['user_id', 'saloon_id']);
 
